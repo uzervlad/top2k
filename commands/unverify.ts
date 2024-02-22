0bc5d6b5224c4e180bf0caa113bd1692609375a3
OctopuSSX <vladik0701@gmail.com>
2024-02-22 21:04:40 +0300

    unverify command

---
+++ commands/unverify.ts
@@ -0,0 +1,21 @@
+import { ChatInputCommandInteraction, PermissionFlagsBits, SlashCommandBuilder, type CacheType } from "discord.js";
+import Command from "../command";
+import type { Context } from "../context";
+import { db } from "../db";
+import { users } from "../schema";
+import { eq } from "drizzle-orm";
+
+export default class UnverifyCommand extends Command {
+  data = new SlashCommandBuilder()
+    .setName('unverify')
+    .setDescription('')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles);
+
+  ephemeral = true;
+
+  async execute(interaction: ChatInputCommandInteraction<CacheType>, context: Context) {
+    await db.delete(users).where(eq(users.id, interaction.user.id));
+
+    interaction.editReply("unverified");
+  }
+}
